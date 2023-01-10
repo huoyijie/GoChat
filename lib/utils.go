7ba30911a9505ba199fa21db10b14ad3ff2afc0e
huoyijie <yijie.huo@foxmail.com>
2023-01-10 15:18:21 +0800

    refactor v0.3 format code

--- lib/utils.go
+++ lib/utils.go
@@ -14,6 +14,7 @@ import (
 var HomeDir = getHomeDir()
 var WorkDir = getWorkDir()
 
+// 返回 home 目录 ~/
 func getHomeDir() (homeDir string) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
@@ -22,6 +23,7 @@ func getHomeDir() (homeDir string) {
 	return
 }
 
+// 返回工作目录 ~/.gochat
 func getWorkDir() (workDir string) {
 	workDir = filepath.Join(HomeDir, ".gochat")
 	if _, err := os.Stat(workDir); os.IsNotExist(err) {
@@ -56,6 +58,7 @@ func PrintMessage(msg *Msg) {
 	fmt.Fprintf(os.Stdout, "%s->%s:%s\n", msg.From, msg.To, msg.Data)
 }
 
+// 打印服务器错误
 func PrintErr(errRes *ErrRes) {
 	fmt.Fprintf(os.Stdout, "系统异常: %d\n", errRes.Code)
 }
